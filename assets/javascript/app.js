5f1baf93be607c2a21d7b8ca6b7d1e57658d0275
Your Name <you@example.com>
2019-05-20 17:44:37 -0400

    first question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,4 +5,33 @@ var choiceB = :
 var choiceC = ;
 var choiceD = ; 
 
-var start = function()
+var start = function();
+
+
+// create questions
+
+var questions = [
+  {
+    question : "When was the first Grand Prix of Monaco?",
+    imgSrc : something.png,
+    choiceA : "Correct", 
+    choiceB : "Wrong",
+    choiceC : "Wrong",
+    choiceD : "Wrong",
+    correct : "A"
+  }
+
+
+
+
+
+
+
+
+       
+]
+
+// create choices
+// create timer 
+// create submit button
+// create page with results. 
