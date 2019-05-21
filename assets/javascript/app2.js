d5c287fc7a8352989f4cf530d4ee97717c41ef96
Your Name <you@example.com>
2019-05-21 12:59:27 -0400

    rounded corners, submit button, containers padding

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,8 +1,7 @@
 
 // set variables
 // when button start clicked run the timer
-// Show all 5 questions with answers 
-// make answers buttons
+// answer button correct/incorrect
 // submit button 
 // stop the timer
 // show results 
@@ -15,10 +14,8 @@ var correctA = 0;
 var wrongA = 0;
 var buttonStart = 0;
 
+
 // when button start clicked run the timer
-
-
-
 $(document).ready(function(){
     $("#start-button").click(function(){
         // start the timer.
