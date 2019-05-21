3073681999bcf5391b610acbd451d86e69ef4263
Your Name <you@example.com>
2019-05-21 12:45:05 -0400

    questions html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,120 +1,137 @@
 
-// Create variables
+// // Create variables
 
-var count = 0; 
-// 10s
-var questionTime = 10; 
-var timer;
-var score = 0;
-var runningQuestion = 0;
-
-
-
-// const lastQuestion = questions.length - 1;
-// let runningQuestion = 0;
-// let count = 0;
-// const questionTime = 10; // 10s
-// const gaugeWidth = 150; // 150px
-// const gaugeUnit = gaugeWidth / questionTime;
-// let TIMER;
-// let score = 0;
+// var count = 0; 
+// // 10s
+// var questionTime = 10; 
+// var timer;
+// var score = 0;
+// var runningQuestion = 0;
+// var start = $("#start-button");
 
 
-
-// start function 
-// var start = function();
-
-
-// create questions
-
-var questions = [
-  {
-    question : "When was the first Grand Prix of Monaco? (1929)",
-    choiceA : "Correct", 
-    choiceB : "Wrong",
-    choiceC : "Wrong",
-    choiceD : "Wrong",
-    correct : "A"
-
-  }, {
-
-    question : "How many Grand Prix races are there in a season? (21)",
-    choiceA : "Wrong",
-    choiceB : "Wrong",
-    choiceC : "Correct",
-    choiceD : "Wrong",
-    correct : "C"
-
-  }, {
-
-    question: "Who has won the F1 the most times? (Michael Schumacher)",
-    choiceA: "Wrong",
-    choiceB: "Correct",
-    choiceC: "Wrong",
-    choiceD: "Wrong",
-    correct: "B"
-
-  },{
-    question: "Who is the best F1 driver of all time? (Ayrton Senna)",
-    choiceA: "Correct",
-    choiceB: "Wrong",
-    choiceC: "Wrong",
-    choiceD: "Wrong",
-    correct: "A"
-  }, { 
-    question: "Who was the best driver in 2018? (Lewis HAmilton)",
-    choiceA: "Wrong",
-    choiceB: "Wrong",
-    choiceC: "Wrong",
-    choiceD: "Correct",
-    correct: "D"
-    }
-];
-
-// Show question
-
-function showQuestion(){
-    var q = questions[runningQuestion];
-
-    question.innerHtml = "<p>" + q.question + "</p>";
-    choiceA.innerHtml = q.choiceA;
-    choiceB.innerHtml = q.choiceB;
-    choiceC.innerHtml = q.choiceC;
-    choiceD.innerHtml = q.choiceD;
-}
-
-start.addEventListener("click", startQuiz);
-
-// start quiz
-function start() {
-    // start.style.display = "none";
-    renderQuestion();
-    // quiz.style.display = "block";
-    // renderProgress();
-    // renderCounter();
-    TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
-}
+// // const lastQuestion = questions.length - 1;
+// // let runningQuestion = 0;
+// // let count = 0;
+// // const questionTime = 10; // 10s
+// // const gaugeWidth = 150; // 150px
+// // const gaugeUnit = gaugeWidth / questionTime;
+// // let TIMER;
+// // let score = 0;
 
 
 
+// // start function 
+// // var start = function();
 
 
-// checkAnswer
+// // create questions
 
-function checkAnswer(answer) {
-    if (answer === q.correct) {
-        // answer is correct
-        score++;
-    } 
 
-    count = 0;
-    if (runningQuestion < lastQuestion) {
-        runningQuestion++;
-        showQuestion();
-    } else {
-        // end the quiz 
-        clearInterval(TIMER);
-        scoreRender();
-    }
-}
+// var questions = [
+//   {
+//     question : "When was the first Grand Prix of Monaco? (1929)",
+//     choiceA : "1929", 
+//     choiceB : "1982",
+//     choiceC : "1910",
+//     choiceD : "1911",
+//     correct: "choiceA"
 
+
+//   }, {
+
+//     question : "How many Grand Prix races are there in a season? (21)",
+//     choiceA : "Wrong",
+//     choiceB : "Wrong",
+//     choiceC : "Correct",
+//     choiceD : "Wrong",
+//     correct : "choiceC"
+
+//   }, {
+
+//     question: "Who has won the F1 the most times? (Michael Schumacher)",
+//     choiceA: "Wrong",
+//     choiceB: "Correct",
+//     choiceC: "Wrong",
+//     choiceD: "Wrong",
+//     correct: "choiceB"
+
+//   },{
+//     question: "Who is the best F1 driver of all time? (Ayrton Senna)",
+//     choiceA: "Correct",
+//     choiceB: "Wrong",
+//     choiceC: "Wrong",
+//     choiceD: "Wrong",
+//     correct: "choiceA"
+//   }, { 
+//     question: "Who was the best driver in 2018? (Lewis HAmilton)",
+//     choiceA: "Wrong",
+//     choiceB: "Wrong",
+//     choiceC: "Wrong",
+//     choiceD: "Correct",
+//     correct: "choiceD"
+//     }
+// ];
+
+// // Show question
+// function showQuestion(){
+//     var q = questions[runningQuestion];
+
+//     question.innerHtml = "<p>" + q.question + "</p>";
+//     choiceA.innerHtml = q.correct;
+//     choiceB.innerHtml = q.c;
+//     choiceC.innerHtml = q.choiceC;
+//     choiceD.innerHtml = q.choiceD;
+// }
+
+// start.on("click", start);
+
+// // start quiz
+// function start() {
+//     start.style.display = "none";
+//     renderQuestion();
+//     quiz.style.display = "block";
+//     // renderProgress();
+//     // renderCounter();
+//     TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
+// }
+
+
+
+
+
+// // checkAnswer
+
+// function checkAnswer(answer) {
+//     if (answer === q.correct) {
+//         // answer is correct
+//         score++;
+//     } 
+
+//     count = 0;
+//     if (runningQuestion < lastQuestion) {
+//         runningQuestion++;
+//         showQuestion();
+//     } else {
+//         // end the quiz 
+//         clearInterval(TIMER);
+//         scoreRender();
+//     }
+// }
+
+
+// set variables
+var choices = 0;
+var correctA = 0;
+var wrongA = 0;
+var buttonStart = 0;
+
+// when button start clicked run the timer
+
+
+
+$(document).ready(function () {
+    $("#start-button").click(function () {
+        // start the timer.
+    });
+});
---
+++ assets/javascript/app2.js
@@ -0,0 +1,26 @@
+
+// set variables
+// when button start clicked run the timer
+// Show all 5 questions with answers 
+// make answers buttons
+// submit button 
+// stop the timer
+// show results 
+
+
+
+// set variables
+var choices = 0;
+var correctA = 0;
+var wrongA = 0;
+var buttonStart = 0;
+
+// when button start clicked run the timer
+
+
+
+$(document).ready(function(){
+    $("#start-button").click(function(){
+        // start the timer.
+    });
+});
